af9a36a3c818c6d7bfb1defc5a1e63073988500b
KairosFaith <TA_TECH@mages.edu.sg>
2022-08-02 11:13:02 +0800

    Started the project
    edited git attributes LFS too

---
+++ Source/ProjectTT/ProjectTT.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectTT.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectTT, "ProjectTT" );
+
+DEFINE_LOG_CATEGORY(LogProjectTT)
+ 

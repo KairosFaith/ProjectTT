a183cf32ce9f98ba69ff4f13fe7beadac0d936d5
KairosFaith <TA_TECH@mages.edu.sg>
2022-08-07 17:54:58 +0800

    Update ITTselectable.h

--- Source/ProjectTT/ITTselectable.h
+++ Source/ProjectTT/ITTselectable.h
@@ -23,5 +23,5 @@ class PROJECTTT_API IITTselectable
 	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
 public:	
 	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
-		bool Select(); //return false if action is invalid
+		bool Select(FVector chosenSpot); //return false if action is invalid
 };
